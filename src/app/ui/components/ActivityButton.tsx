cccaa7fded06d133a5467229fb99ca8a1127afa5
Kamil Dobrzyński <kamil.dobrzynski@ithero.pl>
2019-10-03 20:10:39 +0200

    Add ActivityButton

---
+++ src/app/ui/components/ActivityButton.tsx
@@ -0,0 +1,31 @@
+import { fontStyles } from '@ui/common-styles';
+import * as React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+interface Props {
+    title: string;
+    onPress(): void;
+}
+
+export const ActivityButton = (props: Props) => {
+    return (
+        <TouchableOpacity style={styles.buttonContainer} onPress={props.onPress}>
+            <Text style={[styles.title, fontStyles.highlighted]}>{props.title}</Text>
+        </TouchableOpacity>
+    );
+};
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        borderRadius: 8,
+        flexDirection: 'row',
+        height: 50,
+        justifyContent: 'center',
+        width: 260,
+    },
+    title: {
+        fontSize: 22,
+    },
+});
