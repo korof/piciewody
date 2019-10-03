f619277e39547d11aaedfe0850fec850bae6b85e
Kamil Dobrzyński <kamil.dobrzynski@ithero.pl>
2019-10-03 20:41:27 +0200

    Add Redux-Persist

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ import * as React from 'react';
 import { AppRegistry } from 'react-native';
 import firebase from 'react-native-firebase';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
 
 import { name as appName } from './app.json';
 import App from './src/app/home/containers/home-view';
 import { store } from './src/app/store';
 
+const rehydrateStore = () => {
+    persistStore(store, null, () => {
+        store.getState();
+    });
+};
+
 class Piciewody extends React.Component {
+
     componentDidMount() {
+        rehydrateStore();
         firebase.auth().signInAnonymously();
     }
 
