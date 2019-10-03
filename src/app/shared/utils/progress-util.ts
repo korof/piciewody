db5b549d1425074a8cc18d61a8b6fdb101a479df
Kamil Dobrzyński <kamil.dobrzynski@ithero.pl>
2019-10-03 21:29:43 +0200

    Add ProgressBar animation

--- src/app/shared/utils/progress-util.ts
+++ src/app/shared/utils/progress-util.ts
@@ -1,3 +1,12 @@
-export const progressUtil = (current: number, max: number) => {
-    return (current / max) * 100;
+export const progressUtil = (current: number, max: number, fixed?: boolean) => {
+    const percentage = ((current / max) * 100).toFixed(0);
+    if (fixed) {
+        if (Number(percentage) <= 100) {
+            return percentage;
+        } else {
+            return 100;
+        }
+    } else {
+        return percentage;
+    }
 };
--- src/app/ui/components/ProgressBar.tsx
+++ src/app/ui/components/ProgressBar.tsx
@@ -9,19 +9,42 @@ interface Props {
 }
 
 export const ProgressBar = (props: Props) => {
+    const _renderValue = (): JSX.Element => (
+        <Text style={styles.value}>{progressUtil(props.current, props.max)} %</Text>
+    );
+
+    const _renderBar = (): JSX.Element => (
+        <View
+            style={[
+                styles.progressBar,
+                { width: `${progressUtil(props.current, props.max, true)}%` },
+            ]}
+        />
+    );
+
     return (
-        <View style={styles.progressContainer}>
-            <Text>{progressUtil(props.current, props.max)}</Text>
+        <View style={styles.mainContainer}>
+            {_renderBar()}
+            {_renderValue()}
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    progressBar: {},
-    progressContainer: {
+    mainContainer: {
         borderColor: GLOBAL_COLORS.silverChalice,
         borderWidth: 1,
         height: 60,
         width: '100%',
+    },
+    progressBar: {
+        backgroundColor: GLOBAL_COLORS.dodgerBlue,
+        height: '100%',
+    },
+    value: {
+        alignSelf: 'center',
+        position: 'absolute',
+        textAlign: 'center',
+        top: 20,
     },
 });
