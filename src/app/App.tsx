9872e712ca91b651c7200a7d4cca5f57785fef78
Kamil Dobrzyński <kamil.dobrzynski@ithero.pl>
2019-10-03 19:40:09 +0200

    Add Header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,37 @@
+import { commonStyles, fontStyles } from '@ui';
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
 export default class App extends React.Component {
+    _renderHeader = (): JSX.Element => (
+        <Text style={styles.headerText}>
+            Each of us needs
+            <Text style={[fontStyles.bold, fontStyles.highlighted]}> 2.5 liters</Text> of water a
+            day!
+        </Text>
+    );
+
+    _renderProgress = (): JSX.Element => <Text>asd</Text>;
+
     render(): JSX.Element {
         return (
-            <View style={styles.mainContainer}>
-                <Text>Hello!</Text>
-            </View>
+            <SafeAreaView style={[styles.mainContainer, commonStyles.padding]}>
+                {this._renderHeader()}
+                {this._renderProgress()}
+            </SafeAreaView>
         );
     }
 }
 
 const styles = StyleSheet.create({
+    headerText: {
+        alignItems: 'center',
+        fontSize: 24,
+        fontWeight: '300',
+        height: 100,
+        justifyContent: 'center',
+        textAlign: 'center',
+    },
     mainContainer: {
         alignItems: 'center',
         flex: 1,
